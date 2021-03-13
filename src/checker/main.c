727405f2745b78a3750ca8e1d96ce30344e25cfd
marcos <marcosviudes@gmail.com>
2021-03-13 01:22:06 +0100

    Starting proyect

---
+++ src/checker/main.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int     main(int argc, char **argv)
+{
+	printf("checker funciona");
+	printf("checker funciona2");
+	return (0);
+}
---
+++ src/push_swap/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int     main(int argc, char **argv)
+{
+	int		i = 0;
+	i++;
+	
+	printf("push_swap funciona");
+	return (0);
+}
